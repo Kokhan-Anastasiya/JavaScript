abe91efa5e7d4ac256391c994cefbfea2a55a671
unknown <kokhannastya@gmail.com>
2022-09-08 13:39:39 +0300

    Updated file JS part 1

--- JS part 1.js
+++ JS part 1.js
@@ -60,7 +60,7 @@ console.log("item_7 == " + typeof(item_7), "item_7_type == " + typeof(item_7_typ
 //  26. Создать переменную “age_1” и присвоить ей значение 10
 //  27. Создать переменную “age_2” и присвоить ей значение 18
 //  28. Создать переменную “age_3” и присвоить ей значение 60
-let age_1 = 15
+let age_1 = 60
 let age_2 = 18
 let age_3 = 60
 
@@ -68,14 +68,67 @@ let age_3 = 60
 //  30. Если age_1 < age_2, вывести в консоль “You don’t have access cause your age is ” + age_1 + “ It’s less then ”
 //  31. Если age_1 >=  age_2 и age_1 <=  age_3, вывести в консоль “Welcome  !”
 //  32. Если age_1  > age_3, вывести в консоль “Keep calm and look Culture channel”.
-//  33. Иначе выводите “Technical work”.
+
 if (age_1 < age_2) {
 console.log(`You don’t have access cause your age is ${age_1}. It’s less then 18`)
 }
-if (age_1 >= age_2 && age_1 < age_3) {
+if (age_1 >= age_2 && age_1 <= age_3) {
     console.log("Welcome!")
 }
 if (age_1 > age_3) {
     console.log("Keep calm and look Culture channel")
 }
+
+// 1*: Преобразовать написанный код в 26-32 пунктах в функцию, принимающую на вход возраст.
+
+// Вывести в консоль результат работы функции с возрастами 17, 18, 61
+const checkAge = function(age) {
+ if (age < 18) {
+    return "You don’t have access cause your age is less then 18"
+ }
+ if (age >= 18 && age <= 60) {
+    return "Welcome!"
+ }
+ if (age > 60) {
+    return "Keep calm and look Culture channel"
+ }
+}
+console.log(checkAge(17))
+console.log(checkAge(18))
+console.log(checkAge(61))
+// 2*: Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
+
+const checkAge1 = function(age) {
+    if (age != Number) {
+        return "Error"
+    }
+    if (age < 18) {
+       return "You don’t have access cause your age is less then 18"
+    }
+    if (age >= 18 && age <= 60) {
+       return "Welcome!"
+    }
+    if (age > 60) {
+       return "Keep calm and look Culture channel"
+    }
+   } 
+   console.log(checkAge1(false))
+
+// 3**: Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
+
+const checkAge2 = function(age) {
+    if (age === String) {
+        return +age
+    }
+    if (age < 18) {
+       return "You don’t have access cause your age is less then 18"
+    }
+    if (age >= 18 && age <= 60) {
+       return "Welcome!"
+    }
+    if (age > 60) {
+       return "Keep calm and look Culture channel"
+    }
+   } 
+   console.log(checkAge2("60"))
 
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+let sign = prompt("What`s your age?")
+   if (sign < 18) {
+    alert("You don’t have access cause your age is less then 18")
+   }
+   if (sign >= 18 && sign <= 60) {
+    alert ("Welcome!")
+   }
+   if (sign > 60) {
+    alert ("Keep calm and look Culture channel")
+   }
+
+console.log(prompt(18))
